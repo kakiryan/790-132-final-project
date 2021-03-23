b6c7d1e5d8c1a55d14ef7e14177a414f84d0a90c
Kaki Ryan <kakiryan6@gmail.com>
2021-03-23 16:52:26 -0400

    Some progress on Integer language.

--- SymbolicExecution.v
+++ SymbolicExecution.v
@@ -25,3 +25,81 @@ Fixpoint eval_pathcond (p : pathcond) : bool :=
   | none       => true
   | Pand be p' => (eval_boolexp be) && (eval_pathcond p')
   end.
+
+
+  (** TODO: Defining the Integer language.
+  - exlusively signed ints
+  - simple assigns
+  - if statements with then/else
+  - go-to labels
+  - way to get inputs (e.g. procedure parameters, global variables, read operations). 
+  - arithmetic expr: +, -, x
+  - bool expr: >= 0 only *)
+
+
+Fixpoint leb (n m : nat) : bool :=
+  match n with
+  | O => true
+  | S n' =>
+      match m with
+      | O => false
+      | S m' => leb n' m'
+      end
+  end.
+
+Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
+
+Inductive Int :=
+  | Zero : Int
+  | Positive: nat -> Int
+  | Negative: nat  -> Int.
+
+Check Negative 4.
+Check Positive 2.
+Check O.
+
+Definition Int_plus (n1 n2: Int) : Int :=
+ match n1, n2 with
+ | Positive n1, Positive n2 => Positive (n1 + n2)
+ | Positive n1, Negative n2 => if n2 <=? n1 then Positive (n1 - n2) else Negative (n2 - n1)
+ | Negative n1, Positive n2 => if n1 <=? n2 then Positive (n2 - n1) else Negative (n1 - n2)
+ | Negative n1, Negative n2 => Negative (n1 + n2)
+ | Zero, x => x
+ | x, Zero => x
+end.
+
+Check Int_plus (Negative 2) (Positive 4).
+Compute Int_plus (Negative 2) (Positive 4).
+Compute Int_plus (Negative 6) (Positive 4).
+Compute Int_plus Zero Zero.
+Compute Int_plus (Negative 5) Zero.
+
+Definition Int_mult (n1 n2: Int) : Int :=
+ match n1, n2 with
+ | Positive n1, Positive n2 => Positive (n1 * n2)
+ | Positive n1, Negative n2 => Negative (n1 * n2)
+ | Negative n1, Positive n2 => Negative (n1 * n2)
+ | Negative n1, Negative n2 => Positive (n1 * n2)
+ | Zero, x => Zero
+ | x, Zero => Zero
+end.
+
+Check Int_mult (Negative 2) (Positive 4).
+Compute Int_mult (Negative 2) (Positive 4).
+Compute Int_mult (Negative 6) (Positive 4).
+Compute Int_mult Zero (Positive 2).
+Compute Int_mult (Negative 5)  (Negative 5).
+
+(* the only condition supported by lang is checking its >= 0 *)
+Definition Int_non_neg (n1 : Int) : boolexp :=
+ match n1 with
+ | Positive n1 => B (true)
+ | Zero => B (true)
+ | Negative n1 => B (false)
+end.
+
+Check Int_non_neg (Positive 3).
+Compute Int_non_neg (Positive 3).
+Compute Int_non_neg (Zero).
+Compute Int_non_neg (Negative 3).
+
