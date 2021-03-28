e9aff9392577f50d81d2befab339b383787e18f0
Matthew <mattyg@live.unc.edu>
2021-03-28 11:27:10 -0400

    Constructing Integer language

--- SymbolicExecution.v
+++ SymbolicExecution.v
@@ -1,11 +1,52 @@
 From Coq Require Import Strings.String.
+From Coq Require Import ZArith.Int.
+(* TODO: Figure out how to use numbers *)
+Import Z_as_Int.
+
 (** We can treat a boolean expression as one object,
     if we wrap it inside this type. *)
+
+(** TODO: 
+    - Figure out how to use numbers
+    - Finish booleval last case
+    - Include program state (potentially with Table from ADT chapter)
+      + Update eval functions to consider state
+      + Update evals with new notation
+    - Make a representation of the symbolic execution tree
+    - Make a program eval function (only with if/else, not loops/goto)
+      + Make a symbolic version:
+        * Takes in program, state, symbolic execution tree, and path condition
+        * For each instruction type, update these in a different way
+        * State maps variable names to expressions
+    - Make notation for new concepts
+*)
+
+Declare Custom Entry intlang.
+Declare Scope intlang_scope.
+
+Inductive intexp : Type :=
+| IntLit (n : nat)
+| IntAdd (n1 n2: intexp)
+| IntSub (n1 n2: intexp)
+| IntMult (n1 n2: intexp).
+(* | IntId (x : string) *)
+
+(* TODO: update function to also consider state *)
+Fixpoint inteval (ie : intexp) : nat :=
+  match ie with
+  | IntLit n => n
+  | IntAdd n1 n2 => (inteval n1) + (inteval n2)
+  | IntSub n1 n2 => (inteval n1) - (inteval n2)
+  | IntMult n1 n2 => (inteval n1) * (inteval n2)
+  end.
+
 Inductive boolexp : Type :=
-  | B (b : bool)
+  | BTrue
+  | BFalse
   | Band (b1 : boolexp) (b2 : boolexp)
   | Bor (b1 : boolexp) (b2 : boolexp)
-  | Bnot (b : boolexp).
+  | Bnot (b : boolexp)
+  | Bge0 (n : intexp).
 
 (** TODO: We want a path condition to be a list of conditions on
     symbolic variables, which isn't reflected here. *)
@@ -15,10 +56,12 @@ Inductive pathcond : Type :=
 
 Fixpoint eval_boolexp (be : boolexp) : bool :=
   match be with
-  | B b => b
+  | BTrue => true
+  | BFalse => false
   | Band b1 b2 => (eval_boolexp b1) && (eval_boolexp b2)
   | Bor b1 b2 => (eval_boolexp b1) || (eval_boolexp b2)
   | Bnot b => negb (eval_boolexp b)
+  | Bge0 n => true
   end.
 
 Fixpoint eval_pathcond (p : pathcond) : bool :=
@@ -37,113 +80,10 @@ Fixpoint eval_pathcond (p : pathcond) : bool :=
   - arithmetic expr: +, -, x
   - bool expr: >= 0 only *)
 
-
-Fixpoint leb (n m : nat) : bool :=
-  match n with
-  | O => true
-  | S n' =>
-      match m with
-      | O => false
-      | S m' => leb n' m'
-      end
-  end.
-
-Notation "x <=? y" := (leb x y) (at level 70) : nat_scope.
-
-Inductive Int :=
-  | Zero : Int
-  | Positive: nat -> Int
-  | Negative: nat  -> Int.
-
-Check Negative 4.
-Check Positive 2.
-Check O.
-
 Definition W : string := "W".
 Definition X : string := "X".
 Definition Y : string := "Y".
 Definition Z : string := "Z".
-
-Inductive intexp : Type :=
-| IntAdd (n1 n2: Int)
-| IntSub (n1 n2: Int)
-| IntMult (n1 n2: Int)
-| IntId (x : string).
-
-
-
-Definition Int_plus (n1 n2: Int) : Int :=
- match n1, n2 with
- | Positive n1, Positive n2 => Positive (n1 + n2)
- | Positive n1, Negative n2 => if n2 <=? n1 then Positive (n1 - n2) else Negative (n2 - n1)
- | Negative n1, Positive n2 => if n1 <=? n2 then Positive (n2 - n1) else Negative (n1 - n2)
- | Negative n1, Negative n2 => Negative (n1 + n2)
- | Zero, x => x
- | x, Zero => x
-end.
-
-Check Int_plus (Negative 2) (Positive 4).
-Compute Int_plus (Negative 2) (Positive 4).
-Compute Int_plus (Negative 6) (Positive 4).
-Compute Int_plus Zero Zero.
-Compute Int_plus (Negative 5) Zero.
-
-
-(* this may not be quite right *) 
-Definition Int_minus (n1 n2: Int) : Int :=
- match n1, n2 with
- | Positive n1, Positive n2 => Positive (n1 - n2)
- | Positive n1, Negative n2 => if n2 <=? n1 then Positive (n1 + n2) else Negative (n2 + n1)
- | Negative n1, Positive n2 => if n1 <=? n2 then Negative (n2 + n1) else Negative (n1 + n2)
- | Negative n1, Negative n2 => Negative (n1 - n2)
- | Zero, Positive n2 => Negative n2
- | Zero, Negative n2 => Positive n2
- | x, Zero => x
-end.
-
-Check Int_minus (Negative 2) (Positive 4).
-Compute Int_minus (Negative 2) (Positive 4).
-Compute Int_minus (Negative 6) (Positive 4).
-Compute Int_minus Zero (Positive 2).
-Compute Int_minus (Negative 5)  (Negative 5).
-
-Definition Int_mult (n1 n2: Int) : Int :=
- match n1, n2 with
- | Positive n1, Positive n2 => Positive (n1 * n2)
- | Positive n1, Negative n2 => Negative (n1 * n2)
- | Negative n1, Positive n2 => Negative (n1 * n2)
- | Negative n1, Negative n2 => Positive (n1 * n2)
- | Zero, x => Zero
- | x, Zero => Zero
-end.
-
-Check Int_mult (Negative 2) (Positive 4).
-Compute Int_mult (Negative 2) (Positive 4).
-Compute Int_mult (Negative 6) (Positive 4).
-Compute Int_mult Zero (Positive 2).
-Compute Int_mult (Negative 5)  (Negative 5).
-
-Definition inteval (a: intexp) : Int :=
- match a with
-  | IntAdd n1 n2 => Int_plus n1 n2
-  | IntSub n1 n2 => Int_minus n1 n2
-  | IntMult n1 n2 => Int_mult n1 n2
-  | IntId x => Positive 2 (* Lol this is arbitrary so it will compile. 
-need to do something like the textbook does with states probably *)
-end.
-
-(* the only condition supported by lang is checking its >= 0 *)
-Definition Int_non_neg (n1 : Int) : boolexp :=
- match n1 with
- | Positive n1 => B (true)
- | Zero => B (true)
- | Negative n1 => B (false)
-end.
-
-Check Int_non_neg (Positive 3).
-Compute Int_non_neg (Positive 3).
-Compute Int_non_neg (Zero).
-Compute Int_non_neg (Negative 3).
 
 (** TODO: Just brainstorming adding notes -- 3/23/21
   - function to visit  each variable in the value store associated with
@@ -155,8 +95,33 @@ Compute Int_non_neg (Negative 3).
 Inductive statement := 
   | assignment (x: string) (a: intexp) (* made up of a LHS loc and a RHS expr to evaluated*)
   | if_stmt (b: boolexp) (c1 c2: statement) (* evaluates to the boolexp defined above *)
-  | go_to. (* how do we ant to define functions? do we want to limit them to be just a name with 2 int params or someting?*)
+  | go_to. (* how do we want to define functions? do we want to limit them to be just a name with 2 int params or someting?*)
 
 
 Notation "'if' x 'then' y 'else' z 'end'" :=
          (if_stmt x y z). (*what level to put at ? *)
+
+Notation "<{ e }>" := e (at level 0, e custom intlang at level 99) : com_scope.
+Notation "( x )" := x (in custom intlang, x at level 99) : com_scope.
+Notation "x" := x (in custom intlang at level 0, x constr at level 0) : com_scope.
+Notation "f x .. y" := (.. (f x) .. y)
+                  (in custom intlang at level 0, only parsing,
+                  f constr at level 0, x constr at level 9,
+                  y constr at level 9) : com_scope.
+Notation "x + y" := (IntAdd x y)
+  (in custom intlang at level 50, left associativity).
+Notation "x - y" := (IntSub x y)
+  (in custom intlang at level 50, left associativity).
+Notation "x * y" := (IntMult x y)
+  (in custom intlang at level 40, left associativity).
+Notation "'true'"  := true (at level 1).
+Notation "'true'"  := BTrue (in custom intlang at level 0).
+Notation "'false'"  := false (at level 1).
+Notation "'false'"  := BFalse (in custom intlang at level 0).
+Notation "x >= 0" := (Bge0 x) (in custom intlang at level 70, no associativity).
+Notation "x && y" := (Band x y)
+  (in custom intlang at level 80, left associativity).
+Notation "x || y" := (Bor x y)
+  (in custom intlang at level 80, left associativity).
+Notation "'~' b"  := (Bnot b)
+  (in custom intlang at level 75, right associativity).
